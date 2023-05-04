00584bf60defc03b8bf08e184ad6a2fc2bc60ecf
zaira fernandez <114684585+zazi479@users.noreply.github.com>
2023-05-04 10:57:50 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package incidencias;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author zazi1
+ */
+public class Main {
+    
+    public void Programa_principal(){
+        
+        ArrayList<Incidencias> incidencias = new ArrayList<>();
+        incidencias.add(new Incidencias("Pendiente", 105, "No tiene acceso a internet"));
+        incidencias.add(new Incidencias("Resuelto", 14, "No arranca", "El equipo no estaba enchufado"));
+        incidencias.add(new Incidencias("Resuelto", 5, "La pantalla se ve rosa", "Cambio del cable VGA"));
+        incidencias.add(new Incidencias("Pendiente", 237, "Hace un ruido extraño"));
+        incidencias.add(new Incidencias("Pendiente", 111, "Se queda colgado al abrir 3 ventanas"));
+       
+           
+    
+          int contadorPendientes=0;
+            for (Incidencias incidencia : incidencias){
+                System.out.println("Puesto:"+ incidencia.getPuesto()+ "-Incidencia" +incidencia.getCodigo()+  "-"+ incidencia.getDescripcion()+ "-"+incidencia.getEstado());
+                    if (incidencia.getEstado().equals("Pendiente")){
+                        contadorPendientes++;
+                   }    
+                        
+            }
+            System.out.println("Incidencias pendientes: " + contadorPendientes);
+        }     
+    
+    
+    
+    public static void main(String[] args) {
+        Main start = new Main();
+        start.Programa_principal();
+    }
+}
